b7e2c569a5eda00d98ccfffd76d08b493e4785d3
Nathan Arthur <nathan@nathanarthur.com>
2023-12-27 17:07:18 -0500

    home message

--- ep_simple_urls/index.js
+++ ep_simple_urls/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.registerRoute = (hookName, args, callback) => {
+exports.expressCreateServer = (hookName, args, callback) => {
   args.app.use((req, res, next) => {
     const isAdmin = req.url.startsWith('/admin/') || req.url === '/admin';
     if (!req.url.startsWith('/p/') && !isAdmin) {
@@ -8,5 +8,15 @@ exports.registerRoute = (hookName, args, callback) => {
     }
     next();
   });
+
   callback();
+};
+
+exports.expressPreSession = async (hookName, args) => {
+  args.app.get('/', (req, res) => {
+    res.send(`
+  <h1>DtherPad: dreeves's EtherPad <br> Also known as hippo.padm.us</h1>
+  <p>(If you don't know how to create new pads, ask <a href="http://ai.eecs.umich.edu/people/dreeves">dreeves</a>.)</p>
+    `);
+  });
 };
