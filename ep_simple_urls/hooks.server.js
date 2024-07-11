70aca66f461bda23c3a23b2ffc856ca93abdfa3a
Nathan Arthur <nathan@nathanarthur.com>
2024-07-11 14:39:17 -0400

    extract expressPreSession

--- ep_simple_urls/hooks.server.js
+++ ep_simple_urls/hooks.server.js
@@ -1,133 +1,15 @@
 "use strict";
 
-const API = require("ep_etherpad-lite/node/db/API");
-const expost = require("expost");
 const eejs = require("ep_etherpad-lite/node/eejs");
-const rewrites = require("./rewrites.json");
 const toolbar = require("ep_etherpad-lite/node/utils/toolbar");
 const settings = require("ep_etherpad-lite/node/utils/Settings");
 const webaccess = require("ep_etherpad-lite/node/hooks/express/webaccess");
-const cheerio = require("cheerio");
-const { shouldRewriteUrl } = require("./lib/shouldRewriteUrl");
+
+const { expressPreSession } = require("./lib/expressPreSession");
 
 const secretDomain = process.env.ETHERPAD_SECRET_DOMAIN;
 
-const getMatchingDomain = (url) => {
-  let target;
-  let statusCode = 301;
-
-  for (const rewrite of rewrites) {
-    if (url.match(rewrite.regex)) {
-      target = rewrite.replace;
-
-      if (rewrite.permanent) {
-        statusCode = 302;
-      }
-
-      if (rewrite.last) {
-        break;
-      }
-    }
-  }
-
-  return { target, statusCode };
-};
-
-const renderPad = async (pad) => {
-  const { text } = await API.getText(pad);
-
-  const body = await expost.parseMarkdown(text, { strict: false });
-  const $ = cheerio.load(body, {
-    xml: {
-      xmlMode: false,
-      decodeEntities: false,
-    },
-  });
-  const title = expost.parseTitle(text);
-  let desc = $.text();
-  if (desc.length > 160) {
-    desc = desc.substring(0, 160);
-    desc += "...";
-  }
-
-  return eejs.require("ep_simple_urls/templates/pad.html", {
-    title,
-    desc,
-    body,
-  });
-};
-
-exports.expressPreSession = async (hookName, args) => {
-  args.app.get("/", async (req, res) => {
-    let padName;
-
-    if (req.hostname === secretDomain) {
-      padName = "public";
-    } else {
-      padName = "expost";
-    }
-
-    try {
-      const renderedPad = await renderPad(padName);
-      return res.send(renderedPad);
-    } catch (err) {
-      console.error(`Failed to render pad ${padName}:`, err);
-      return res.status(404).send("<h1>404 Not Found</h1>");
-    }
-  });
-
-  args.app.use((req, res, next) => {
-    if (req.url === "/post" && req.hostname !== secretDomain) {
-      return res.status(401).send("Unauthorized");
-    }
-
-    next();
-  });
-
-  args.app.use((req, res, next) => {
-    const shouldRewrite = shouldRewriteUrl(req.url);
-
-    if (shouldRewrite) {
-      req.url = `/p${req.url}`;
-    }
-
-    next();
-  });
-
-  args.app.use((req, res, next) => {
-    const { target, statusCode } = getMatchingDomain(req.url);
-
-    if (target) {
-      return res.redirect(statusCode, target);
-    }
-
-    next();
-  });
-
-  args.app.get("/p/:pad", async (req, res, next) => {
-    const { pad } = req.params;
-    const { public: forcePublic } = req.query;
-    const shouldShowEditor =
-      req.hostname === secretDomain || forcePublic !== "true";
-
-    if (shouldShowEditor) {
-      next();
-    } else {
-      try {
-        const renderedPad = await renderPad(pad);
-        return res.send(renderedPad);
-      } catch (err) {
-        console.error(`Failed to render pad ${pad}`);
-        console.error(err);
-        return res.status(404).send("<h1>404 Not Found</h1>");
-      }
-    }
-  });
-
-  args.app.get("/api/404", (req, res) => {
-    res.status(404).send("<h1>404 Not Found</h1>");
-  });
-};
+exports.expressPreSession = expressPreSession;
 
 exports.socketio = (hookName, args, callback) => {
   const io = args.io;
