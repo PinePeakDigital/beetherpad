cd583a513285752b5b494f3e864ed28d3b037535
Nathan Arthur <nathan@nathanarthur.com>
2024-07-11 15:43:24 -0400

    add redirect test for /api/404

--- ep_simple_urls/lib/getRedirect.spec.js
+++ ep_simple_urls/lib/getRedirect.spec.js
@@ -7,4 +7,9 @@ describe("getRedirect", () => {
     const result = getRedirect("/foo");
     expect(result).toEqual(null);
   });
+
+  it("does not redirect /api/404", () => {
+    const result = getRedirect("/api/404");
+    expect(result).toEqual(null);
+  });
 });
