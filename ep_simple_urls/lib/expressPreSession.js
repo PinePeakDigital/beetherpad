d98f090932cd4db397f6273b0b577ba305c31b07
Nathan Arthur <nathan@nathanarthur.com>
2024-07-11 16:11:15 -0400

    extract renderPad

--- ep_simple_urls/lib/expressPreSession.js
+++ ep_simple_urls/lib/expressPreSession.js
@@ -1,35 +1,8 @@
 "use strict";
 
 const { shouldRewriteUrl } = require("./shouldRewriteUrl");
-const cheerio = require("cheerio");
-const API = require("ep_etherpad-lite/node/db/API");
-const expost = require("expost");
-const eejs = require("ep_etherpad-lite/node/eejs");
 const { getRedirect } = require("./getRedirect");
-
-const renderPad = async (pad) => {
-  const { text } = await API.getText(pad);
-
-  const body = await expost.parseMarkdown(text, { strict: false });
-  const $ = cheerio.load(body, {
-    xml: {
-      xmlMode: false,
-      decodeEntities: false,
-    },
-  });
-  const title = expost.parseTitle(text);
-  let desc = $.text();
-  if (desc.length > 160) {
-    desc = desc.substring(0, 160);
-    desc += "...";
-  }
-
-  return eejs.require("ep_simple_urls/templates/pad.html", {
-    title,
-    desc,
-    body,
-  });
-};
+const { renderPad } = require("./renderPad");
 
 const expressPreSession = async (hookName, args) => {
   const secretDomain = process.env.ETHERPAD_SECRET_DOMAIN;
