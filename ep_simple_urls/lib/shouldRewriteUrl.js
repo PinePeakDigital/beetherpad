8ba8bda49ea94336aaa0dce6443065a9c64ca36c
Nathan Arthur <nathan@nathanarthur.com>
2024-07-11 16:36:21 -0400

    shouldRewriteUrl default false

--- ep_simple_urls/lib/shouldRewriteUrl.js
+++ ep_simple_urls/lib/shouldRewriteUrl.js
@@ -3,15 +3,17 @@
 const shouldRewriteUrl = (path) => {
   const segments = path.split("/").filter(Boolean);
   const blacklist0 = ["admin", "admin-auth", "health", "post", "static", "p"];
-  const whitelist1 = ["export"];
 
-  if (whitelist1.includes(segments[1])) return true;
+  if (path.startsWith("/p/")) return false;
   if (blacklist0.includes(segments[0])) return false;
-  if (segments.length > 2) return false;
   if (path.includes(".")) return false;
-  if (path === "/api/404") return false;
 
-  return true;
+  if (segments.length === 1) return true;
+  if (path.includes("/export/txt")) return true;
+  if (path.includes("/timeslider")) return true;
+
+  // Defaulting to false should be safer
+  return false;
 };
 
 module.exports = {
